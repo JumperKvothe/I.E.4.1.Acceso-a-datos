b6f3f4035a8c43dc99b3d1f73b362960b9b59922
qiqer <qiqe.rionegro@hotmail.es>
2018-10-16 10:44:32 +0200

    3º

---
+++ src/Colegio/Main.java
@@ -0,0 +1,14 @@
+package Colegio;
+
+import com.db4o.Db4oEmbedded;
+import com.db4o.ObjectContainer;
+
+public class Main {
+
+	public static void main(String[] args) {
+		
+		ObjectContainer bd=Db4oEmbedded.openFile(Db4oEmbedded.newConfiguration(), "colegio.db4o");
+
+	}
+
+}
---
+++ src/POJOs/Centro.java
@@ -0,0 +1,8 @@
+package POJOs;
+
+public class Centro {
+
+	private String nomnbre;
+    private String tipo_org;
+    private int num_personas;
+}
